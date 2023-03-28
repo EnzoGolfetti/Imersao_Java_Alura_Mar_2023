262256666ec4e1f7838040787cac98d9df846cb0
Enzo Golfetti <enzo.golfetti04@gmail.com>
2023-03-27 21:11:11 -0300

    Commit_Aula_1_Imersao_Java_Alura

---
+++ Alura_Stickers_EG_2023/src/App.java
@@ -0,0 +1,22 @@
+import java.net.http.HttpClient; //Faz o import do HttpClient
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    
+        //Acessar via HTTP a API do IMDB através de um GET
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json"
+        HttpClient
+
+        //Buscar os top 250 filmes da plataforma
+        //Guardar a resposta da API em uma string
+        //Filtrar e extrair apenas as informações que sejam de interesse do projeto
+        //Título - Poster - Rating (Parsing)
+        //Exibir e manipular os dados
+
+
+
+
+    
+    }
+}
